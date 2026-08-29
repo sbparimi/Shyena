f62925a4640693e5067b47972d0fbe1888548b26
sbparimi <49089369+sbparimi@users.noreply.github.com>
2026-08-29 13:13:03 +0200

    feat: expose SAGE runtime API

---
+++ src/lib/sage/index.ts
@@ -0,0 +1,4 @@
+export { createSageGraph } from "./graph";
+export { evaluateQualityGate } from "./quality-gate";
+export { createPublicationManifest } from "./manifest";
+export type * from "./types";
