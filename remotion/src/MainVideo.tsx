292e9369c357e99ac03e0dd37deb8c709ffe01e0
sbparimi <49089369+sbparimi@users.noreply.github.com>
2026-08-30 23:39:18 +0200

    Embed original Shyena soundtrack in hero video

--- remotion/src/MainVideo.tsx
+++ remotion/src/MainVideo.tsx
@@ -1,9 +1,10 @@
-import { AbsoluteFill } from "remotion";
+import { AbsoluteFill, Audio, staticFile } from "remotion";
 import { SceneAssuranceFlow } from "./scenes/SceneAssuranceFlow";
 import { BRAND } from "./theme";
 
 export const MainVideo: React.FC = () => (
   <AbsoluteFill style={{ backgroundColor: BRAND.ink }}>
     <SceneAssuranceFlow />
+    <Audio src={staticFile("shyena-soundtrack.wav")} volume={0.28} />
   </AbsoluteFill>
 );
