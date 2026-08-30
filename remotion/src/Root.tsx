ca5cdacfee98c1931ee6a7d4992bc86915509b08
sbparimi <49089369+sbparimi@users.noreply.github.com>
2026-08-30 23:14:33 +0200

    Set explainer video duration to 63 seconds

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -5,7 +5,7 @@ export const RemotionRoot: React.FC = () => (
   <Composition
     id="main"
     component={MainVideo}
-    durationInFrames={1758}
+    durationInFrames={1890}
     fps={30}
     width={1920}
     height={1080}
