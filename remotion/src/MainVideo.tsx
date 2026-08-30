1451f1533f455a50d40805ece0b645131a09abd8
sbparimi <49089369+sbparimi@users.noreply.github.com>
2026-08-30 23:14:29 +0200

    Replace hero video with Cognigy assurance flow explainer

--- remotion/src/MainVideo.tsx
+++ remotion/src/MainVideo.tsx
@@ -1,38 +1,9 @@
 import { AbsoluteFill } from "remotion";
-import { TransitionSeries, springTiming } from "@remotion/transitions";
-import { fade } from "@remotion/transitions/fade";
-import { loadFont as loadInter } from "@remotion/google-fonts/Inter";
-import { loadFont as loadSerif } from "@remotion/google-fonts/InstrumentSerif";
-import { SceneOffice } from "./scenes/SceneOffice";
-import { SceneExpert } from "./scenes/SceneExpert";
-import { SceneVerdict } from "./scenes/SceneVerdict";
-import { SceneEnd } from "./scenes/SceneEnd";
+import { SceneAssuranceFlow } from "./scenes/SceneAssuranceFlow";
 import { BRAND } from "./theme";
-
-loadInter("normal", { weights: ["400", "500", "700", "800"], subsets: ["latin"] });
-loadSerif("normal", { weights: ["400"], subsets: ["latin"] });
-
-const timing = springTiming({ config: { damping: 200 }, durationInFrames: 20 });
 
 export const MainVideo: React.FC = () => (
   <AbsoluteFill style={{ backgroundColor: BRAND.ink }}>
-    <TransitionSeries>
-      <TransitionSeries.Sequence durationInFrames={343}>
-        <SceneOffice />
-      </TransitionSeries.Sequence>
-      <TransitionSeries.Transition presentation={fade()} timing={timing} />
-      <TransitionSeries.Sequence durationInFrames={752}>
-        <SceneExpert />
-      </TransitionSeries.Sequence>
-      <TransitionSeries.Transition presentation={fade()} timing={timing} />
-      <TransitionSeries.Sequence durationInFrames={411}>
-        <SceneVerdict />
-      </TransitionSeries.Sequence>
-      <TransitionSeries.Transition presentation={fade()} timing={timing} />
-      <TransitionSeries.Sequence durationInFrames={312}>
-        <SceneEnd />
-      </TransitionSeries.Sequence>
-    </TransitionSeries>
+    <SceneAssuranceFlow />
   </AbsoluteFill>
 );
-
