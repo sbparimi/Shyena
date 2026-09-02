fc4470c3e800f338937e751ddbca0bb9deb24517
sbparimi <49089369+sbparimi@users.noreply.github.com>
2026-09-02 22:29:58 +0200

    Restore original Shyena falcon brand logo

--- src/components/site/logo.tsx
+++ src/components/site/logo.tsx
@@ -1,4 +1,4 @@
 import { Link } from "@tanstack/react-router";
 
 // Original Shyena falcon logo artwork supplied for the brand.
-const ORIGINAL_LOGO = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU
+const ORIGINAL_LOGO = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAkGBwgHBgkIBwgKCgkLDRYPDQw
